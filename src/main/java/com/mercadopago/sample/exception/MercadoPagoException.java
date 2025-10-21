6b4fb764278f4ef12a7dd2de430c0c1475ac8146
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2025-10-21 01:29:49 -0300

    Update MercadoPagoException.java

--- src/main/java/com/mercadopago/sample/exception/MercadoPagoException.java
+++ src/main/java/com/mercadopago/sample/exception/MercadoPagoException.java
@@ -4,4 +4,8 @@ public class MercadoPagoException extends RuntimeException {
     public MercadoPagoException(String message) {
         super(message);
     }
+
+    public MercadoPagoException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
