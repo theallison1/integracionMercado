86b87a1bb722c73d8c083be2197580e8a778b487
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2025-10-27 20:15:47 -0300

    Update RefundService.java

--- src/main/java/com/mercadopago/sample/service/RefundService.java
+++ src/main/java/com/mercadopago/sample/service/RefundService.java
@@ -17,6 +17,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.List;
+    import java.util.ArrayList;
 
 @Service
 public class RefundService {
