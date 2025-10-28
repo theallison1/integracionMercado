4807c8e7b77e8e19479fc881d952a1c709a4e240
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2025-10-27 22:10:51 -0300

    Update MercadoPagoInitializer.java

--- src/main/java/com/mercadopago/sample/config/MercadoPagoInitializer.java
+++ src/main/java/com/mercadopago/sample/config/MercadoPagoInitializer.java
@@ -1,3 +1,16 @@
+package com.mercadopago.sample.config;
+
+import com.mercadopago.sample.service.MercadoPagoStoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
 // En MercadoPagoInitializer.java - REEMPLAZAR las líneas problemáticas
 @PostConstruct
 public void initialize() {
