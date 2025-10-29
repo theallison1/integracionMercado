ae4e1970bac75c88bb4a060cedcddb9f178f8ee9
francoastorgaseg <francoastorga.seg@gmail.com>
2025-10-29 16:18:55 -0300

    Delete src/main/resources/static/js/config.js

--- src/main/resources/static/js/config.js
+++
@@ -1,9 +0,0 @@
-export const mercadoPagoConfig = {
-    getPublicKey: () => document.getElementById("mercado-pago-public-key").value,
-    locale: 'es-AR'
-};
-
-export const customerFormConfig = {
-    requiredFields: ['firstName', 'lastName', 'email'],
-    dniTypes: ['DNI', 'CUIT', 'CUIL']
-};
