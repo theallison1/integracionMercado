6182bc9a34db41e98f84bd7fd75d65868c079372
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2025-11-04 16:19:37 -0300

    Update RestTemplateConfig.java

--- src/main/java/com/mercadopago/sample/config/RestTemplateConfig.java
+++ src/main/java/com/mercadopago/sample/config/RestTemplateConfig.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
+// ✅ AGREGAR ESTE IMPORT:
+import com.mercadopago.sample.util.MercadoPagoHeaderLogger;
 import java.util.Collections;
 
 @Configuration
