b7cbae7611b783c19f7bcc45b528cb2faf795e3a
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2025-11-04 15:57:05 -0300

    Create RestTemplateConfig.java

---
+++ src/main/java/com/mercadopago/sample/config/RestTemplateConfig.java
@@ -0,0 +1,22 @@
+package com.mercadopago.sample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+@Configuration
+public class RestTemplateConfig {
+    
+    @Autowired
+    private MercadoPagoHeaderLogger headerLogger;
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(headerLogger));
+        return restTemplate;
+    }
+}
